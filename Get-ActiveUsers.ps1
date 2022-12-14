58581138d3a9503087eb61896fb12d6f788032f1
Artur Gawrych <arthur.gawrych@gmail.com>
2022-12-14 09:30:30 +0000

    new add

---
+++ Get-ActiveUsers.ps1
@@ -0,0 +1,5 @@
+
+$TimeStamp3M = (Get-Date).AddMonths(-3)
+$ActiveUsers = @(Get-ADUser -Filter * -Properties * | Where-Object { ($_.Enabled -eq $True) -and ($_.LastLogonDate -gt $TimeStamp3M) })
+
+Write-Output "Number of Active Users logged in at least once in the past three months: $($ActiveUsers.Count)"
